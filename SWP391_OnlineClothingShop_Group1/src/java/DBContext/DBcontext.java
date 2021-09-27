3e4886fa019d10ac1ab705a89ebb1895022fcd91
chaubnmhe153019 <chaubnmhe153019@fpt.edu.vn>
2021-09-27 20:48:55 +0700

    edit DBcontext

--- SWP391_OnlineClothingShop_Group1/src/java/DBContext/DBcontext.java
+++ SWP391_OnlineClothingShop_Group1/src/java/DBContext/DBcontext.java
@@ -5,12 +5,57 @@
  */
 package DBContext;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author BEAN
  */
 public class DBcontext {
-    public static void main(String[] args) {
-        System.out.println("bean");
-    }
+        public static Connection open()
+        {
+            try {
+                String server="localhost";
+                String databasename="htshop";
+                String username="sa";
+                String password="123456";
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                return DriverManager.getConnection("jdbc:sqlserver://"+server+":1433;databaseName="+databasename,username,password);
+
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(DBcontext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return null;
+        }
+        public static void close(Connection conn,Statement stmt,ResultSet rs)
+        {
+            if(stmt!=null)
+            {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DBcontext.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(conn!=null){
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DBcontext.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if(rs!=null){
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DBcontext.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }   
 }
