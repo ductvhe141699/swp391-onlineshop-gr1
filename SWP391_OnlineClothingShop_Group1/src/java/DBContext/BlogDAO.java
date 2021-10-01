5a3577e302ba3e02d865f1118f76b0d93ea116ac
Mai <maintnhe151402@fpt.edu.vn>
2021-10-01 21:24:35 +0700

    BlogDAO

--- SWP391_OnlineClothingShop_Group1/src/java/DBContext/BlogDAO.java
+++ SWP391_OnlineClothingShop_Group1/src/java/DBContext/BlogDAO.java
@@ -24,21 +24,77 @@ public class BlogDAO {
     private Connection conn;
     private PreparedStatement ps;
     private ResultSet rs;
+    private String query;
 
     public ArrayList<Blog> getAllBlogs() {
 
         try {
             ArrayList<Blog> blogList = new ArrayList<>();
-            String query = "select * from Blog";
+            query = "select * from Blog  ";
             conn = new DBcontext().open();
             ps = conn.prepareStatement(query);
             rs = ps.executeQuery();
             while (rs.next()) {
                 blogList.add(new Blog(rs.getInt("ID"), rs.getString("Author"), rs.getString("Title"), rs.getString("Content"), rs.getString("imageLink")));
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
         }
 
         return null;
+    }
+
+    public Blog getBlogByID(int id) {
+        query = "SELECT * FROM Blog WHERE ID = ?";
+        try {
+            conn = new DBcontext().open();
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Blog(rs.getInt("ID"), rs.getString("Author"), rs.getString("Title"), rs.getString("Content"), rs.getString("imageLink"));
+            }
+        } catch (SQLException e) {
+        }
+        return null;
+    }
+    public void add(String author,String title, String content, String imageLink ) {
+        query = "INSERT INTO Blog VALUES (?,?,?,?);";
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setString(1, author);
+            ps.setString(2, title);
+            ps.setString(3, content);
+            ps.setString(4, imageLink);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+        }
+    }
+        public void update(String author,String title, String content, String imageLink, String id) {
+        query = "Update Blog\n"
+                + "SET Author= ?\n"
+                + "Title =?,\n"
+                + "Content=?,\n"
+                + "imageLink=?,\n"
+                + "Where ID =?";
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setString(1,author);
+            ps.setString(2, title);
+            ps.setString(3, content);
+            ps.setString(4, imageLink);
+            ps.setString(5, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+        }
+    }
+            public void delete(int id) { 
+        String query = "Delete FROM Blog WHERE ID = ?";
+
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, id);        
+            ps.executeUpdate();
+        } catch (SQLException e) {
+        }
     }
 }
