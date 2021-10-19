550d9150fef6379960e188dc59b4fa38c1d03274
phunghe150108 <phunghe150108@fpt.edu.vn>
2021-10-19 21:08:00 +0700

    Update ProfileControl.java

--- SWP391_OnlineClothingShop_Group1/src/java/controller/ProfileControl.java
+++ SWP391_OnlineClothingShop_Group1/src/java/controller/ProfileControl.java
@@ -9,6 +9,7 @@ import entity.Users;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author Admin
  */
+@WebServlet(name = "ProfileControl", urlPatterns = {"/ProfileControl"})
 public class ProfileControl extends HttpServlet {
 
     /**
