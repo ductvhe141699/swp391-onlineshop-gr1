b7608f13db17067bf4f2f01833ce89b1fa6a5376
chaubnmhe153019 <chaubnmhe153019@fpt.edu.vn>
2021-10-21 19:01:55 +0700

    add ShipInfoDAO ShipInfo entity

--- SWP391_OnlineClothingShop_Group1/src/java/entity/ShipInfo.java
+++ SWP391_OnlineClothingShop_Group1/src/java/entity/ShipInfo.java
@@ -10,5 +10,71 @@ package entity;
  * @author BEAN
  */
 public class ShipInfo {
+    private int orderID;
+    private String CustomerName;
+    private String ShippingAddress;
+    private int ShipCityID;
+    private String PhoneNum;
+    private String Note;
+
+    public ShipInfo() {
+    }
+
+    public ShipInfo(int orderID, String CustomerName, String ShippingAddress, int ShipCityID, String PhoneNum, String Note) {
+        this.orderID = orderID;
+        this.CustomerName = CustomerName;
+        this.ShippingAddress = ShippingAddress;
+        this.ShipCityID = ShipCityID;
+        this.PhoneNum = PhoneNum;
+        this.Note = Note;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getCustomerName() {
+        return CustomerName;
+    }
+
+    public void setCustomerName(String CustomerName) {
+        this.CustomerName = CustomerName;
+    }
+
+    public String getShippingAddress() {
+        return ShippingAddress;
+    }
+
+    public void setShippingAddress(String ShippingAddress) {
+        this.ShippingAddress = ShippingAddress;
+    }
+
+    public int getShipCityID() {
+        return ShipCityID;
+    }
+
+    public void setShipCityID(int ShipCityID) {
+        this.ShipCityID = ShipCityID;
+    }
+
+    public String getPhoneNum() {
+        return PhoneNum;
+    }
+
+    public void setPhoneNum(String PhoneNum) {
+        this.PhoneNum = PhoneNum;
+    }
+
+    public String getNote() {
+        return Note;
+    }
+
+    public void setNote(String Note) {
+        this.Note = Note;
+    }
     
 }
