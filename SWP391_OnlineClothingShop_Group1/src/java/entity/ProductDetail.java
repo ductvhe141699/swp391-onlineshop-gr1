dea096b44468d3cc46059998f6883452b81d2766
ManhndHE141170 <90176001+ManhndHE141170@users.noreply.github.com>
2021-11-01 10:09:22 +0700

    Add files via upload

---
+++ SWP391_OnlineClothingShop_Group1/src/java/entity/ProductDetail.java
@@ -0,0 +1,184 @@
+package entity;
+
+import java.util.List;
+
+/**
+ *
+ * @author Duy Manh
+ */
+public class ProductDetail {
+    int productID ;
+    String productName ; 
+    String  desc ;
+    double originPrice ; 
+    double sellPrice ; 
+    double  salePercent ; 
+    int subCateID ; 
+    int sellerID ; 
+    int amount ; 
+    int statusID ; 
+    int brandID ;
+    double height ;
+    double width ;
+    double weight  ;
+    String url ;
+    List<String> images;
+
+    public ProductDetail() {
+    }
+
+    public ProductDetail(int productID, String productName, String desc, double originPrice, double sellPrice, double salePercent, int subCateID, int sellerID, int amount, int statusID, int brandID, double height, double width, double weight, String url, List<String> list) {
+        this.productID = productID;
+        this.productName = productName;
+        this.desc = desc;
+        this.originPrice = originPrice;
+        this.sellPrice = sellPrice;
+        this.salePercent = salePercent;
+        this.subCateID = subCateID;
+        this.sellerID = sellerID;
+        this.amount = amount;
+        this.statusID = statusID;
+        this.brandID = brandID;
+        this.height = height;
+        this.width = width;
+        this.weight = weight;
+        this.url = url;
+        this.images = list;
+        
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public double getOriginPrice() {
+        return originPrice;
+    }
+
+    public void setOriginPrice(double originPrice) {
+        this.originPrice = originPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public double getSalePercent() {
+        return salePercent;
+    }
+
+    public void setSalePercent(double salePercent) {
+        this.salePercent = salePercent;
+    }
+
+    public int getSubCateID() {
+        return subCateID;
+    }
+
+    public void setSubCateID(int subCateID) {
+        this.subCateID = subCateID;
+    }
+
+    public int getSellerID() {
+        return sellerID;
+    }
+
+    public void setSellerID(int sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getStatusID() {
+        return statusID;
+    }
+
+    public void setStatusID(int statusID) {
+        this.statusID = statusID;
+    }
+
+    public int getBrandID() {
+        return brandID;
+    }
+
+    public void setBrandID(int brandID) {
+        this.brandID = brandID;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    
+    
+    
+    
+
+
+}
