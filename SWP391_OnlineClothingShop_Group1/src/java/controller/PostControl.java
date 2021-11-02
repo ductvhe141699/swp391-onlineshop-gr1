f535d559d14a86e47d2954c04ac3b6aba4da6a11
phunghe150108 <phunghe150108@fpt.edu.vn>
2021-11-03 01:01:26 +0700

    Update PostControl.java

--- SWP391_OnlineClothingShop_Group1/src/java/controller/PostControl.java
+++ SWP391_OnlineClothingShop_Group1/src/java/controller/PostControl.java
@@ -7,6 +7,7 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,18 +31,12 @@ public class PostControl extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PostControl</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PostControl at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        PostDAO pdao = new PostDAO();
+           List<Post> postList = pdao.getAllPost();
+           
+           request.setAttribute("postList", postList);
+           request.getRequestDispatcher("PostList.jsp").forward(request, response);
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
